ecd840ade31d7866b5fbb6c5fac084c4058f1dab
Jean Bernuy <jean.bernuy@bbva.com>
2021-07-29 20:56:30 -0500

    include recyclerview and adapters

--- app/src/main/java/com/jeanbernuy/cookingapp/ui/ListRecipeFragment.kt
+++ app/src/main/java/com/jeanbernuy/cookingapp/ui/ListRecipeFragment.kt
@@ -8,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.jeanbernuy.cookingapp.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeanbernuy.cookingapp.core.Resource
+import com.jeanbernuy.cookingapp.databinding.FragmentMainBinding
+import com.jeanbernuy.cookingapp.ui.adapters.RecipeAdapter
 import com.jeanbernuy.cookingapp.ui.viewmodel.ListRecipeViewModel
 
 /**
@@ -19,24 +22,41 @@ import com.jeanbernuy.cookingapp.ui.viewmodel.ListRecipeViewModel
  */
 class ListRecipeFragment : Fragment() {
 
-    private val viewModel:ListRecipeViewModel by viewModels()
+    private val viewModel: ListRecipeViewModel by viewModels()
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
     }
-    
+
     private fun setupViews() {
-        viewModel.listRecipes.observe(viewLifecycleOwner, Observer{
-            Log.d("Response",it.toString())
+
+        binding.rvRecipes.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvRecipes.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        viewModel.listRecipes.observe(viewLifecycleOwner, Observer {result ->
+            when (result){
+                is Resource.Success -> {
+                    binding.rvRecipes.adapter = RecipeAdapter(requireContext(), result.data.recipeCollection()!!.items())
+                }
+            }
         })
     }
-    
+
 }
---
+++ app/src/main/java/com/jeanbernuy/cookingapp/ui/adapters/RecipeAdapter.kt
@@ -0,0 +1,42 @@
+package com.jeanbernuy.cookingapp.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jeanbernuy.cookingapp.R
+import com.jeanbernuy.cookingapp.RecipeQuery
+import com.jeanbernuy.cookingapp.core.BaseViewHolder
+import com.jeanbernuy.cookingapp.databinding.RecipeRowBinding
+
+class RecipeAdapter(
+    private val context: Context,
+    private val listaRecipes: List<RecipeQuery.Item?>
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding = RecipeRowBinding.inflate(LayoutInflater.from(context), parent, false)
+        return MainViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when (holder) {
+            is RecipeAdapter.MainViewHolder -> holder.bind(listaRecipes[position], position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listaRecipes.size
+    }
+
+    inner class MainViewHolder(val binding: RecipeRowBinding) :
+        BaseViewHolder<RecipeQuery.Item?>(binding.root) {
+        override fun bind(item: RecipeQuery.Item?, position: Int) {
+            Glide.with(context).load(item?.photo()?.url()).centerCrop()
+                .placeholder(R.drawable.ic_launcher_background).into(binding.imvRecipe)
+            binding.txtTitle.text = item?.title()
+        }
+
+    }
+}
